3478c5f47b46ff902cb9e88ca73c32da8ae76ad3
Michael North <michael.l.north@gmail.com>
2017-09-07 23:13:54 -0700

    tests for accounts

--- exercises/accounts/tests/accounts.test.ts
+++ exercises/accounts/tests/accounts.test.ts
@@ -1,5 +1,5 @@
 import { AccountManager } from '../src/account-manager';
-import expect from 'expect';
+import * as expect from 'expect';
 
 test('AccountManager is available as a named export from ./src/account-manager.ts', () => {
   expect(AccountManager).toBeDefined();
@@ -7,9 +7,35 @@ test('AccountManager is available as a named export from ./src/account-manager.t
 
 test('Registering a new user', () => {
   let am = new AccountManager();
-  expect(am.register('mike@example.com', '123456Seven')).toBeDefined();
+  let newUser = am.register('mike@example.com', '123456Seven');
+  expect(newUser).toBeDefined();
+  expect(newUser.email).toEqual('mike@example.com');
+  expect(newUser.password).toEqual('123456Seven');
+});
 
-  expect(() => {
-    am.register('', '123456Seven');
-  }).toThrow();
+test('Activating a new user', () => {
+  let am = new AccountManager();
+  let admin = { email: 'lisa@example.com', password: '23456Seven', isActive: true, adminSince: new Date()};
+  let newUser = am.register('mike@example.com', '123456Seven');
+  let activatedUser = am.activateNewUser(admin, newUser);
+  
+  expect(activatedUser).toBeDefined();
+  expect(activatedUser.email).toEqual('mike@example.com');
+  expect(activatedUser.password).toEqual('123456Seven');
+  expect(activatedUser.isActive).toBe(true);
+});
+
+test('Promoting an activated user to admin', () => {
+  let am = new AccountManager();
+  let admin = { email: 'lisa@example.com', password: '23456Seven', isActive: true, adminSince: new Date()};
+  let newUser = am.register('mike@example.com', '123456Seven');
+  let activatedUser = am.activateNewUser(admin, newUser);
+  let newAdmin = am.promoteToAdmin(admin, activatedUser);
+  
+  expect(newAdmin).toBeDefined();
+  expect(newAdmin.email).toEqual('mike@example.com');
+  expect(newAdmin.password).toEqual('123456Seven');
+  expect(newAdmin.isActive).toBe(true);
+  expect(typeof newAdmin.adminSince).toBe('object');
+  expect(typeof newAdmin.adminSince.toISOString).toBe('function');
 });
