a6d66db1c1ccaf4d400ad37c076085389fdd2807
Michael North <michael.l.north@gmail.com>
2017-09-13 18:38:27 -0500

    Color-functions: add type definitions

--- exercises/color-functions/src/index.ts
+++ exercises/color-functions/src/index.ts
@@ -4,10 +4,10 @@ export let color = {
   r: 255,
   g: 0,
   b: 0,
-  get hex() {
+  get hex() : string {
     return rgbToHex(this.r, this.g, this.b);
   },
-  set hex(hex) {
+  set hex(hex: string) {
     let { r, g, b } = hexToRgb(hex);
     this.r = r;
     this.g = g;
