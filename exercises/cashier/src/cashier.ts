25079f144a2966d472f3e87b8d86b1838a670001
Michael North <michael.l.north@gmail.com>
2017-09-14 16:46:31 -0500

    Exercise 4

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,38 @@
-export function cashier() {
-  return {};
+interface CartItem {
+  name: string;
+  price: number;
+  qty: number;
+}
+
+interface CartAPI {
+  length: number;
+  total: number;
+  addItem(item: CartItem): CartAPI;
+  add(name: string, price: number, qty: number): CartAPI;
+}
+
+export function cashier(): CartAPI {
+  let items: CartItem[] = [];
+  return {
+    get length() {
+      return items.reduce((acc, cartItem) => {
+        return acc + cartItem.qty;
+      }, 0);
+    },
+    get total() {
+      return items.reduce((acc, cartItem) => {
+        return acc + (cartItem.qty * cartItem.price);
+      }, 0);
+    },
+    addItem(item): CartAPI {
+      items.push(item);
+      return this;
+    },
+    add(name, price, qty=1): CartAPI {
+      items.push({
+        name, price, qty
+      });
+      return this;
+    }
+  };
 }
