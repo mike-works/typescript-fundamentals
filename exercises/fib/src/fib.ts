8c2c515a5036b031fcedbdd8dc3a99bac9f5c29f
Michael North <michael.l.north@gmail.com>
2017-09-15 10:52:15 -0500

    Exercise 6: fibonacci

--- exercises/fib/src/fib.ts
+++ exercises/fib/src/fib.ts
@@ -4,5 +4,14 @@
  * to generate numbers in the Fibonacci Sequence
  */
 export function* getFibSequence(): IterableIterator<number> {
- 
+  let twoAgo = 1; // 0  1
+  let oneAgo = 0; // 1  1
+  while(true) {
+    let nextValue = twoAgo + oneAgo;
+    yield nextValue;
+    twoAgo = oneAgo;
+    oneAgo = nextValue;
+  }
 }
+
+// 1 1 2
