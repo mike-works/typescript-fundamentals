c80dc298fe00e5ad587713c49f44d257dfd2c7b4
Michael North <michael.l.north@gmail.com>
2017-09-15 15:04:14 -0500

    Exercise 9

--- exercises/autocomplete-3/src/autocomplete.ts
+++ exercises/autocomplete-3/src/autocomplete.ts
@@ -1,13 +1,13 @@
 import { wait } from './utils/promise';
 import { fetchPlaceSummaries, fetchPlaceDetails, PlaceDetails, PlaceSummary } from './utils/places';
 
-export async function autocomplete(term: string): Promise<PlaceDetails[]> {
+export async function autocomplete(term: string) {
   console.log(`⏳ Beginning search for ${term}`);
   // Begin actual query API call
-  let placeResults: PlaceSummary[] = await fetchPlaceSummaries(term);
+  let placeResults = await fetchPlaceSummaries(term);
   console.log(`✅ Completed search for ${term}`);
   let placeIds = placeResults.map(place => place.place_id);
-  let places: PlaceDetails[] = await fetchPlaceDetails(placeIds);
+  let places = await fetchPlaceDetails(placeIds);
   console.log(places);
   // Return the results (eventual value of the task)
   return places;
