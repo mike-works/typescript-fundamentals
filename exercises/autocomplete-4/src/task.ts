b8d7f41e670f11e72961f41a62d25e93f36e7d87
Michael North <michael.l.north@gmail.com>
2017-09-16 21:26:58 -0700

    exercise 10

--- exercises/autocomplete-4/src/task.ts
+++ exercises/autocomplete-4/src/task.ts
@@ -1,4 +1,9 @@
-import { isPromise } from './utils/promise';
+import { isPromise, wait } from './utils/promise';
+
+export interface CancellablePromise<T> extends PromiseLike<T> {
+  cancelled?: boolean;
+}
+
 /**
  * Given a generator function that yields one or more
  * promises, chain them together in sequence
@@ -6,10 +11,42 @@ import { isPromise } from './utils/promise';
  * @param {any} genFn generator function that yields one or more promises
  * @return {undefined}
  */
-export function task<T>(genFn: () => IterableIterator<any>): Promise<T> {
+export function task<T>(genFn: () => IterableIterator<any>): CancellablePromise<T> {
   let p = new Promise<T>((resolve) => {
-    let iterator = genFn(); // Get the iterator
-    // TODO: implement your solution here
-  });
+    let it = genFn(); // Get the iterator
+    let value: any;
+    function nextStep(lastPromiseVal: any) {
+      let itResult = it.next(lastPromiseVal);
+      if (itResult.done && typeof itResult.value === 'undefined') {
+        resolve(value as T);
+        return;
+      } else {
+        value = itResult.value;
+        if (isPromise(value)) {
+          value.then((promiseResult: any) => {
+            if (!p.cancelled)
+              nextStep(promiseResult);
+          });
+          // value is a promise
+        } else {
+          // value is not a promise
+          nextStep(value);
+        }
+      }
+    }
+    nextStep(undefined); // just the first time, this is ok
+  }) as CancellablePromise<T>;
+  p.cancelled = false;
   return p;
 }
+
+// TEST with VS Code "code runner extension"
+task(function*() {
+  let first = yield wait(500).then(() => 'FIRST');
+  console.log('first', first);
+  let second = yield wait(500).then(() => 'SECOND');
+  console.log('second', second);
+  return 'third';
+}).then(lastVal => {
+  console.log('------->', lastVal);
+});
