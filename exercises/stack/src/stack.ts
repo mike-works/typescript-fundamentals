2bb73911d2abef1c3bac12aaeca4e518c0273748
Michael North <michael.l.north@gmail.com>
2017-10-27 16:38:09 -0700

    exercise 5

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -7,5 +7,22 @@ interface IStack<T> {
 }
 
 export class Stack<T> {
-
+  private data: T[] = []
+  push(item: T): IStack<T>
+  push(item: T[]): IStack<T>
+  push(item: T | T[]): IStack<T> {
+    if (item instanceof Array) {
+      this.data.push(...item);
+    } else {
+      this.data.push(item);
+    }
+    return this;
+  }
+  pop(): T | undefined {
+    return this.data.pop();
+  }
+  length() {return this.data.length; }
+  print(): void {
+    console.log(this.data.join(', '));
+  }
 }
