d8bf5e5bcbd20b0f9a0325c5ee0c2b303ba0ebe4
Michael North <michael.l.north@gmail.com>
2017-10-27 15:04:27 -0700

    exercise 3

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -12,6 +12,45 @@ function shuffleArray(a: any[]) {
   }
 }
 
-export class Dealer {
+export enum Suit {
+  Clubs,
+  Diamonds,
+  Hearts,
+  Spades
+}
 
+export enum CardNumber {
+  Ace, Two, Three, Four, Five, Six,
+  Seven, Eight, Nine, Ten,
+  Jack, Queen, King
+}
+
+type Card = [Suit, CardNumber];
+
+export class Dealer {
+  cards: Card[];
+  constructor() {
+    this.cards = this.generateDeck();
+    shuffleArray(this.cards);
+  }
+  generateDeck(): Card[] {
+    let deck: Card[] = [];
+    for (let s = 0; s < Object.keys(Suit).length/2; s++) {
+      for (let n = 0; n < Object.keys(CardNumber).length/2; n++) {
+        deck.push([s, n]);
+      }
+    }
+    return deck;
+  }
+  getLength() {
+    return this.cards.length;
+  }
+  dealHand(numCards: number): Card[] {
+    if (numCards < 0) throw new Error('Hand me YOUR cards');
+    if (numCards > this.cards.length) throw new Error('I do not have enough');
+    return this.cards.splice(0, numCards);
+  }
+  readCard(c: Card): string {
+    return `${CardNumber[c[1]]} of ${Suit[c[0]]}`;
+  }
 }
