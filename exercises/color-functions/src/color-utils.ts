59270df37d6f4947f3a0d535197fbf7aa78566cf
Michael North <michael.l.north@gmail.com>
2017-10-27 13:21:04 -0700

    exercise 1

--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,4 +1,29 @@
-//TODO: Implement hexToRgb
 
+
+//TODO: Implement hexToRgb
+export function hexToRgb(hexString: string): { r: number, g: number, b: number } {
+  if (hexString.length === 3) {
+    // 3 character string
+    let hr = hexString[0];
+    let hg = hexString[1];
+    let hb = hexString[2];
+    return hexToRgb(`${hr}${hr}${hg}${hg}${hb}${hb}`);
+  }
+  // 6 character string
+  let [ r, g, b] = [0, 2, 4]
+    .map(offset => hexString.substring(offset, offset + 2))
+    .map(x => parseInt(x, 16));
+  return { r, g, b };
+}
 
 //TODO: Implement rgbToHex
+export function rgbToHex(r: number, g: number, b: number): string {
+  return [ r, g, b ]
+    .map(rawNum => Math.max(0, Math.min(255, rawNum)))
+    .map(cc => cc.toString(16))
+    .map(x => x.length === 1 ? `0${x}` : x)
+    .join('')
+}
+
+let x: string = rgbToHex(255, 0, 0);
+let { r, g, b } = hexToRgb('f00');
