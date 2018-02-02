a27d8dbf60d15fe5c641b6710794b447007f111e
Michael North <michael.l.north@gmail.com>
2018-02-02 14:13:11 -0800

    Stack

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,28 @@ interface IStack<T> {
   print(): void;
 }
 
-// export class Stack<T> {
-
-// }
+export class Stack<T> implements IStack<T> {
+  private arr: Array<T>;
+  constructor() {
+    this.arr = new Array<T>();
+  }
+  length(): number {
+    return this.arr.length;
+  }
+  pop(): T | undefined {
+    return this.arr.pop();
+  }
+  print(): void {
+    console.log(JSON.stringify(this.arr));
+  }
+  push(item: T): IStack<T>
+  push(items: T[]): IStack<T>
+  push(itemOrItems: T | T[]): IStack<T> {
+    if (itemOrItems instanceof Array) {
+      this.arr.push(...itemOrItems);
+    } else {
+      this.arr.push(itemOrItems)
+    }
+    return this;
+  }
+}
