f4abc3d65da395104b01ce735ef77b976a656598
Michael North <michael.l.north@gmail.com>
2018-02-01 16:44:53 -0800

    Exercise 2: Account Manager

--- exercises/accounts/src/account-manager.ts
+++ exercises/accounts/src/account-manager.ts
@@ -1,3 +1,17 @@
+export interface IUser {
+  email: string;
+  password: string;
+  isActive: boolean;
+}
+
+export interface IActiveUser extends IUser {
+  isActive: true
+}
+
+export interface IAdmin extends IActiveUser {
+  adminSince: Date;
+}
+
 export class AccountManager {
   users = new Array();
 
@@ -8,10 +22,10 @@ export class AccountManager {
    * @return the new user account. An admin must activate it using activateNewUser
    * @see this.activateNewUser
    */
-  register(email, password) {
+  register(email: string, password: string): IUser {
     if(!email) throw 'Must provide an email';
     if(!password) throw 'Must provide a password';
-    let user = { email, password };
+    let user = { email, password, isActive: false };
     this.users.push(user);
     return user;
   }
@@ -22,10 +36,9 @@ export class AccountManager {
    * @param userToApprove Newly-registered user, who is to be activated
    * @return the updated user object, now activated
    */
-  activateNewUser(approver, userToApprove) {
+  activateNewUser(approver: IAdmin, userToApprove: IUser): IActiveUser {
     if (!approver.adminSince) throw "Approver is not an admin!";
-    userToApprove.isActive = true;
-    return userToApprove;
+    return Object.assign(userToApprove, { isActive: (true as true) });
   }
 
   /**
@@ -34,10 +47,9 @@ export class AccountManager {
    * @param user an active user who you're making an admin
    * @return the updated user object, now can also be regarded as an admin
    */
-  promoteToAdmin(existingAdmin, user) {
+  promoteToAdmin(existingAdmin: IAdmin, user: IActiveUser) {
     if (!existingAdmin.adminSince) throw "Not an admin!";
-    if (user.isActive !== true) throw "User must be active in order to be promoted to admin!";
-    user.adminSince = new Date();
-    return user;
+    // if (user.isActive !== true) throw "User must be active in order to be promoted to admin!";
+    return Object.assign(user, { adminSince: new Date() });
   }
 }
