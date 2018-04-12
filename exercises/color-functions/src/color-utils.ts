c87ca18915d72eabda14d7b884bd9448946c0ae5
gness1804 <graham.nessler@gmail.com>
2018-04-12 18:41:15 -0500

    Initial setup with failing tests for color-functions

--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,4 +1,15 @@
 //TODO: Implement hexToRgb
+const rgbToHex = (foo: number): boolean => {
+  return false;
+}
 
+const hexToRgb = (foo: string): boolean => {
+  return true;
+};
 
 //TODO: Implement rgbToHex
+
+export {
+  rgbToHex,
+  hexToRgb,
+};
