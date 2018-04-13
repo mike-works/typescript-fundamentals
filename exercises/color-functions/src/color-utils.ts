09d71d5ed5d688704fd90e3eb032acaf06d8f776
gness1804 <graham.nessler@gmail.com>
2018-04-12 19:14:26 -0500

    Got first passing test

--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,13 +1,19 @@
 //TODO: Implement hexToRgb
-const rgbToHex = (foo: number): boolean => {
-  return false;
-}
+// hexToRgb('00ff00') => {r: 0, g: 255, b: 0}
 
 const hexToRgb = (foo: string): boolean => {
   return true;
 };
 
 //TODO: Implement rgbToHex
+// rgbToHex(255, 0, 0) => 'ff0000'
+
+const rgbToHex = (r: number, g: number, b: number): string => {
+  const red = r.toString(16) === '0' ? '00' : r.toString(16);
+  const green = g.toString(16) === '0' ? '00' : g.toString(16);
+  const blue = b.toString(16) === '0' ? '00' : b.toString(16);
+  return `${red}${green}${blue}`;
+}
 
 export {
   rgbToHex,
