7d93d0b702ee66d6735140ab6d2a2c80506146a2
gness1804 <graham.nessler@gmail.com>
2018-04-12 19:32:57 -0500

    Got hexToRgb test to pass

--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,8 +1,27 @@
 //TODO: Implement hexToRgb
-// hexToRgb('00ff00') => {r: 0, g: 255, b: 0}
+// hexToRgb('ff0000') => {r: 255, g: 0, b: 0}
+// hexToRgb('f00') => { r: 255, g: 0, b: 0 }
 
-const hexToRgb = (foo: string): boolean => {
-  return true;
+const hexToRgb = (hex: string): object => {
+  const result = {};
+  if (hex.length === 3) {
+    const red = parseInt(`${hex[0]}${hex[0]}`, 16);
+    Object.assign(result, { r: red});
+    const green = parseInt(`${hex[1]}${hex[1]}`, 16);
+    Object.assign(result, { g: green });
+    const blue = parseInt(`${hex[2]}${hex[2]}`, 16);
+    Object.assign(result, { b: blue });
+  } else if (hex.length === 6) {
+    const red = parseInt(`${hex[0]}${hex[1]}`, 16);
+    Object.assign(result, { r: red });
+    const green = parseInt(`${hex[2]}${hex[3]}`, 16);
+    Object.assign(result, { g: green });
+    const blue = parseInt(`${hex[4]}${hex[5]}`, 16);
+    Object.assign(result, { b: blue });
+  } else {
+    throw new Error('Invalid hex value entered.');
+  }
+  return result;
 };
 
 //TODO: Implement rgbToHex
