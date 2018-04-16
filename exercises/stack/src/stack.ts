5f1ae2ccf7fb97eb0a4343713e19ed1f3be46c16
Mike North <michael.l.north@gmail.com>
2018-04-16 15:38:13 -0700

    Ex5

--- exercises/stack/src/stack.ts
+++ exercises/stack/src/stack.ts
@@ -6,6 +6,31 @@ interface IStack<T> {
   print(): void;
 }
 
-// export class Stack<T> {
+export class Stack<T, S> implements IStack<T> {
+  private items: T[] = [];
+  push(item: T): IStack<T>;
+  push(items: T[]): IStack<T>;
+  push(itemOrItems: T | T[]): IStack<T> {
+    if (itemOrItems instanceof Array) {
+      this.items.push(...itemOrItems);
+    } else {
+      this.items.push(itemOrItems);
+    }
+    return this;
+  }
+  pop(): T | undefined {
+    return this.items.pop();
+  }
+  length(): number {
+    return this.items.length;
+  }
+  print(): void {
+    console.log(JSON.stringify(this.items, null, '  '));
+  }
+}
 
-// }
+let stk = new Stack<string>();
+stk.push(['foo', 'bar']);
+stk.push('baz');
+console.log(`Length: ${stk.length()}`);
+stk.print();
