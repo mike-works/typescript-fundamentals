97ab476fc9c6a35a5c0e41bd023c409e5b350b45
gness1804 <graham.nessler@gmail.com>
2018-04-19 19:34:53 -0500

    Create all the cards

--- exercises/dealer/src/dealer.ts
+++ exercises/dealer/src/dealer.ts
@@ -23,15 +23,65 @@
    Jack,
    Queen,
    King,
+   Joker,
  }
 
- type Card = [Suit, CardNumber];
+ export type Card = [Suit, CardNumber];
 
- const sample: Card[] = [
-   [Suit.Spades, CardNumber.Ace],
- ];
-
- const test:Card = [0, 6];
+const cards: Card[] = [
+  [Suit.Clubs, CardNumber.Ace],
+  [Suit.Clubs, CardNumber.Two],
+  [Suit.Clubs, CardNumber.Three],
+  [Suit.Clubs, CardNumber.Four],
+  [Suit.Clubs, CardNumber.Five],
+  [Suit.Clubs, CardNumber.Six],
+  [Suit.Clubs, CardNumber.Seven],
+  [Suit.Clubs, CardNumber.Eight],
+  [Suit.Clubs, CardNumber.Nine],
+  [Suit.Clubs, CardNumber.Jack],
+  [Suit.Clubs, CardNumber.Queen],
+  [Suit.Clubs, CardNumber.King],
+  [Suit.Clubs, CardNumber.Joker],
+  [Suit.Diamonds, CardNumber.Ace],
+  [Suit.Diamonds, CardNumber.Two],
+  [Suit.Diamonds, CardNumber.Three],
+  [Suit.Diamonds, CardNumber.Four],
+  [Suit.Diamonds, CardNumber.Five],
+  [Suit.Diamonds, CardNumber.Six],
+  [Suit.Diamonds, CardNumber.Seven],
+  [Suit.Diamonds, CardNumber.Eight],
+  [Suit.Diamonds, CardNumber.Nine],
+  [Suit.Diamonds, CardNumber.Jack],
+  [Suit.Diamonds, CardNumber.Queen],
+  [Suit.Diamonds, CardNumber.King],
+  [Suit.Diamonds, CardNumber.Joker],
+  [Suit.Hearts, CardNumber.Ace],
+  [Suit.Hearts, CardNumber.Two],
+  [Suit.Hearts, CardNumber.Three],
+  [Suit.Hearts, CardNumber.Four],
+  [Suit.Hearts, CardNumber.Five],
+  [Suit.Hearts, CardNumber.Six],
+  [Suit.Hearts, CardNumber.Seven],
+  [Suit.Hearts, CardNumber.Eight],
+  [Suit.Hearts, CardNumber.Nine],
+  [Suit.Hearts, CardNumber.Jack],
+  [Suit.Hearts, CardNumber.Queen],
+  [Suit.Hearts, CardNumber.King],
+  [Suit.Hearts, CardNumber.Joker],
+  [Suit.Spades, CardNumber.Ace],
+  [Suit.Spades, CardNumber.Two],
+  [Suit.Spades, CardNumber.Three],
+  [Suit.Spades, CardNumber.Four],
+  [Suit.Spades, CardNumber.Five],
+  [Suit.Spades, CardNumber.Six],
+  [Suit.Spades, CardNumber.Seven],
+  [Suit.Spades, CardNumber.Eight],
+  [Suit.Spades, CardNumber.Nine],
+  [Suit.Spades, CardNumber.Jack],
+  [Suit.Spades, CardNumber.Queen],
+  [Suit.Spades, CardNumber.King],
+  [Suit.Spades, CardNumber.Joker],
+];
 
 function shuffleArray(cards: Card[]) {
   // Iterate over the array
@@ -45,11 +95,15 @@ function shuffleArray(cards: Card[]) {
 export class Dealer {
   cards: Card[];
 
-  constructor(cards: Card[]) {
-    this.cards = cards || [];
+  constructor() {
+    this.cards = cards;
   }
 
   readCard = (card: Card): string => {
     return `${CardNumber[card[1]]} of ${Suit[card[0]]}`;
+  }
+
+  getLength = (): number => {
+    return this.cards.length;
   }
 }
