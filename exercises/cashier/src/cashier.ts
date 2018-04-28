197c7f59e0cc117c34b2260ed8030ca3c348d4ec
gness1804 <graham.nessler@gmail.com>
2018-04-27 20:57:09 -0500

    Add getters

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,7 +1,6 @@
 interface Item {
   name: string,
   price: number,
-  quantity: number,
 }
 
 const items: Item[] = [];
@@ -9,11 +8,30 @@ const items: Item[] = [];
 export const cashier = () => {
   return {
     add(name: string, price: number, quantity?: number):void {
-      items.push({
-        name,
-        price,
-        quantity: quantity || 1,
-      });
-    }
+      if (quantity) {
+        for (let i = 0; i < quantity; i++) {
+          items.push({
+            name,
+            price,
+          });
+        }
+      } else {
+        items.push({
+          name,
+          price,
+        });
+      }
+    },
+    get length(): number {
+      return items.length;
+    },
+    get total(): number {
+      let total = 0;
+      for (let i = 0; i < items.length; i++) {
+        const element = items[i];
+        total += element.price;
+      }
+      return total;
+    },
   };
 }
