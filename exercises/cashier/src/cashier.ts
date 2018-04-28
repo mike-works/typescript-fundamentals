6d4c80ea7d36555c4895a0461ce817443c92e1b1
gness1804 <graham.nessler@gmail.com>
2018-04-27 20:30:14 -0500

    create add method

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,19 @@
-// export function cashier() {
-//   return {};
-// }
+interface Item {
+  name: string,
+  price: number,
+  quantity: number,
+}
+
+const items: Item[] = [];
+
+export const cashier = () => {
+  return {
+    add(name: string, price: number, quantity?: number):void {
+      items.push({
+        name,
+        price,
+        quantity: quantity || 1,
+      });
+    }
+  };
+}
