ef90720ecc25e51409a90a3eee14bf2c2bf086b5
gness1804 <graham.nessler@gmail.com>
2018-04-27 21:41:21 -0500

    Got all except final test passing

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -7,12 +7,45 @@ interface CartItem {
 interface CartAPI {
   length: number,
   total: number,
-  addItem: (obj: CartItem) => CartAPI,
-  add: (name: string, price: number, qty: number) => CartAPI,
+  addItem: (obj: CartItem) => void,
+  add: (name: string, price: number, qty?: number) => void,
 }
 
 export const cashier = (): CartAPI => {
+  const items: CartItem[] = [];
   return {
-
+    get length(): number {
+      if (items.length > 0) {
+        let total = 0;
+        for (let i = 0; i < items.length; i++) {
+          const element: CartItem = items[i];
+          total += element.qty;
+        }
+        return total;
+      }
+      return 0;
+    },
+    get total(): number {
+      if (items.length > 0) {
+        let total = 0;
+        for (let i = 0; i < items.length; i++) {
+          const element: CartItem = items[i];
+          total += (element.price * element.qty);
+        }
+        return total;
+      }
+      return 0;
+    },
+    addItem(obj) {
+      items.push(obj);
+    },
+    add(name, price, qty = 1) {
+      const it: CartItem = {
+        name,
+        price,
+        qty,
+      };
+      items.push(it);
+    },
   };
 }
