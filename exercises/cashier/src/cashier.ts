11a7b7fa9c903dc3de6477ded13e98a3ebaf022e
gness1804 <graham.nessler@gmail.com>
2018-04-27 21:11:15 -0500

    Scrap old stuff and set up basic scaffolding

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,37 +1,18 @@
-interface Item {
+interface CartItem {
   name: string,
   price: number,
+  qty: number,
 }
 
-const items: Item[] = [];
+interface CartAPI {
+  length: number,
+  total: number,
+  addItem: (obj: CartItem) => CartAPI,
+  add: (name: string, price: number, qty: number) => CartAPI,
+}
 
-export const cashier = () => {
+export const cashier = (): CartAPI => {
   return {
-    add(name: string, price: number, quantity?: number):void {
-      if (quantity) {
-        for (let i = 0; i < quantity; i++) {
-          items.push({
-            name,
-            price,
-          });
-        }
-      } else {
-        items.push({
-          name,
-          price,
-        });
-      }
-    },
-    get length(): number {
-      return items.length;
-    },
-    get total(): number {
-      let total = 0;
-      for (let i = 0; i < items.length; i++) {
-        const element = items[i];
-        total += element.price;
-      }
-      return total;
-    },
+
   };
 }
