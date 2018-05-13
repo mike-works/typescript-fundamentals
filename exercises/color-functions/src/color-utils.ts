8a2f2aeae2c4a6ea2175925d3c1cadf80c0354f9
Krista <krista.abraham@gmail.com>
2018-05-13 17:48:13 -0400

    Complete rgbToHex function

--- exercises/color-functions/src/color-utils.ts
+++ exercises/color-functions/src/color-utils.ts
@@ -1,22 +1,19 @@
 //TODO: Implement hexToRgb
 export const hexToRgb = (hex: string): {r: number, g: number, b: number} => {
-  let result = {};
   if (hex.length === 3) {
     hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-  } 
-  result.r = parseInt(hex.slice(0,2), 16);
-  result.g = parseInt(hex.slice(2,4), 16);
-  result.b = parseInt(hex.slice(4,6), 16);
-  return result;
+  }; 
+  let r = parseInt(hex.slice(0,2), 16);
+  let g = parseInt(hex.slice(2,4), 16);
+  let b = parseInt(hex.slice(4,6), 16);
+  return { r, g, b };
 }; 
 
 //TODO: Implement rgbToHex
 export const rgbToHex = (r: number, g: number, b: number): string => {
-  let result = '';
-  result += (r.toString(16).length < 2) ? 0 + r.toString(16) : r.toString(16);
-  result += (g.toString(16).length < 2) ? 0 + g.toString(16) : g.toString(16);
-  result += (b.toString(16).length < 2) ? 0 + b.toString(16) : b.toString(16);
-  return result;
+  return [r, g, b].map((colorNum) => Math.max(0, Math.min(colorNum, 255)))
+    .map((num) => (num.toString(16).length < 2) ? 0 + num.toString(16) : num.toString(16))
+    .join('')
 };
 
 
