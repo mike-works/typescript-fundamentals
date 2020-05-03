5dd6477166dbad9abf41701a864d386a212e269b
rachelfranklin1 <rachel.franklin@marvelapp.com>
2020-05-03 15:51:23 +0100

    feat: start of compiler configuration

--- examples/hello-ts/src/index.ts
+++ examples/hello-ts/src/index.ts
@@ -1,11 +1,8 @@
-/**
- * Create a promise that resolves after some time
- * @param n number of milliseconds before promise resolves
- */
+//  * Create a promise that resolves after some time
+//  * @param n number of milliseconds before promise resolves
 function timeout(n: number) {
-  return new Promise(res => setTimeout(res, n));
+  return new Promise((res) => setTimeout(res, n));
 }
-
 /**
  * Add three numbers
  * @param a first number
@@ -15,7 +12,6 @@ export async function addNumbers(a: number, b: number) {
   await timeout(500);
   return a + b;
 }
-
 //== Run the program ==//
 (async () => {
   console.log(await addNumbers(3, 4));
